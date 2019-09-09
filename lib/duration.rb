8397e45225f064dcacf558c815f1c7fa6a59352f
Sara Shah Baig <sarashahbaig@gmail.com>
2019-09-08 20:03:03 -0700

    code for duration/range

--- lib/duration.rb
+++ lib/duration.rb
@@ -1,23 +1,20 @@
 require "date"
-require "pry"
 
 module HotelBooking
   class Duration
-    attr_reader :id, :start_date, :end_date
+    attr_reader :start_date, :end_date
 
-    def initialize(id:, start_date:, end_date:)
-      @id = id
+    def initialize(start_date:, end_date:)
       @start_date = convert_to_date(start_date)
       @end_date = convert_to_date(end_date)
 
-      # if @end_date <= @start_date
-      #   raise ArgumentError.new "Invaid dates provided"
-      # end
+      if @end_date <= @start_date
+        raise ArgumentError.new "Invaid dates provided"
+      end
     end
 
     def convert_to_date(date_str)
       date_arr = date_str.split("-")
-      # binding.pry
       year = date_arr[0].to_i
       month = date_arr[1].to_i
       date = date_arr[2].to_i
@@ -25,9 +22,9 @@ module HotelBooking
       return Date.new(year, month, date)
     end
 
-    def get_duration()
-      duration = @end_date - @start_date
-      return duration
+    def get_nights()
+      night_count = (@start_date...@end_date).count
+      return night_count
     end
 
     def overlap?(duration)
---
+++ test/duration_test.rb
@@ -0,0 +1,32 @@
+require_relative "test_helper"
+
+describe HotelBooking::Duration do
+  describe "duration test" do
+    it "raise exception when an invalid date range is provided" do
+      expect {
+        HotelBooking::Duration.new(
+          start_date: "2019-01-01",
+          end_date: "2019-01-01",
+        )
+      }.must_raise ArgumentError
+    end
+  end
+  describe "" do
+    before do
+      @duration = HotelBooking::Duration.new(
+        start_date: "2019-09-01",
+        end_date: "2019-09-05",
+      )
+    end
+
+    it "get nights" do
+      start_date = Date.new(2019, 9, 1)
+      end_date = Date.new(2019, 9, 5)
+      count = (start_date...end_date).count
+      night_count = @duration.get_nights()
+      expect(night_count).must_equal count
+    end
+  end
+end
+# describe "cost" do
+#     it "a"
